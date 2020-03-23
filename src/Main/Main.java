702ab809d2ac4b381a5f783bbde4c676a90d150f
Rick Haffer <rickhaffer1992@gmail.com>
2020-03-22 22:18:53 -0600

    First two FXML done

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -9,7 +9,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/View/AddPartScreen.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/View/MainScreen.fxml"));
         primaryStage.setTitle("Inventory Management System");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();

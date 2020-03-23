3e8ade4e63c61369fc4f28a80efaa3be0d363498
Rick Haffer <rickhaffer1992@gmail.com>
2020-03-22 22:03:13 -0600

    First two FXML done

--- src/View/MainScreenController.java
+++ src/View/MainScreenController.java
@@ -4,4 +4,6 @@ public class MainScreenController {
     public void clickButton(){
         System.out.println("You clicked me so hard.");
     }
+
+    public void exitButton() {System.exit(0);}
 }
